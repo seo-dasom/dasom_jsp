04d74364c1945b3781be6ba3c686c2de06eac4bb
서다솜 <somying@naver.com>
2021-06-22 15:39:16 +0900

    파일업로드 예시

--- web_spring/src/main/java/com/web/som/board/controller/BoardController.java
+++ web_spring/src/main/java/com/web/som/board/controller/BoardController.java
@@ -1,9 +1,14 @@
 package com.web.som.board.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.web.som.board.dto.BoardDTO;
@@ -78,18 +84,66 @@ public class BoardController {
     }
 	
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String write(Model m, @ModelAttribute BoardDTO dto) throws Exception {
-    	String forward = "";
+    public String write(Model m, @ModelAttribute BoardDTO dto,
+    		@RequestParam MultipartFile[] file,
+    		HttpServletRequest req) throws Exception {
+    	String forward = "board/add";
+    	String origin_name = "";
+    	String change_name = "";
+    	String file_ext = "";
+    	ArrayList<String> permit_ext = new ArrayList<String>();
+    	permit_ext.add("zip");	permit_ext.add("png");
     	
-    	System.out.println(dto.getTitle());
-    	boolean res = board.add(dto);
-    	
-    	if(res) {
-    		forward = "redirect:/board/detail?id=" + dto.getId();
+    	if(file.length <= 5) {
+	    	for(MultipartFile f: file) {
+	    		if(!f.isEmpty()) {
+	    			if(f.getSize() <= 10 * 1024 * 1024) {
+	    				// 범용 고유 식별자 128 bit
+	    				UUID uuid = UUID.randomUUID();
+	    				
+	    				origin_name = f.getOriginalFilename();
+	    				// 실제로 저장할 파일명 / 파일 다운로드 받을 때 사용해야 할 주소
+	    				change_name = uuid.toString() + "_" + origin_name;
+	    				file_ext = FilenameUtils.getExtension(f.getOriginalFilename());
+	    				
+	    				System.out.println("원본 파일명 : " + origin_name);
+	    				System.out.println("변경된 파일명 : " + change_name);
+		    			System.out.println("확장자 : " + file_ext);
+		    			System.out.println("파일 크기(바이트) : " + f.getSize());
+	    				
+	    				if(permit_ext.contains(file_ext)) {
+	    					System.out.println("허용되는 확장자 입니다.");
+	    					
+	    					// 파일 저장
+	    					String root_path = req.getServletContext().getRealPath("/");
+	    					File save_path = new File(root_path + "/WEB-INF/resources/file/");
+	    					if(!save_path.exists()) {
+	    						// 파일을 저장하기 위한 경로가 존재하지 않으면 생성
+	    						Files.createDirectories(save_path.toPath());
+	    					}
+	    					f.transferTo(new File(save_path + "/" + origin_name));
+	    				} else {
+	    					System.out.println("해당 확장자는 업로드 할 수 없습니다.");
+	    				}
+		    			
+	    			} else {
+	    				System.out.println("업로드 파일의 크기가 큽니다.");
+	    			}
+	    		}
+	    	}
     	} else {
-    		m.addAttribute("data", dto);
-    		forward = "board/add";
+    		System.out.println("파일 업로드 수량 초과");
     	}
+    	
+    	// 파일 업로드 관련 기능 완료 후 주석 해제 필요!
+//    	boolean res = board.add(dto);
+//    	
+//    	if(res) {
+//    		forward = "redirect:/board/detail?id=" + dto.getId();
+//    	} else {
+//    		m.addAttribute("data", dto);
+//    		forward = "board/add";
+//    	}
     	
         return forward;
     }
--- web_spring/src/main/webapp/WEB-INF/views/board/add.jsp
+++ web_spring/src/main/webapp/WEB-INF/views/board/add.jsp
@@ -6,10 +6,31 @@
 <head>
 <meta charset="UTF-8">
 <title>게시글 추가</title>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </head>
+<script type="text/javascript">
+	function fileCheck() {
+		alert("업로드 파일 수 : " + $("#id_file1").get(0).files.length);
+		alert("업로드 파일 이름 : " + $("#id_file1").get(0).files[0].name);
+		alert("업로드 파일 크기 : " + $("#id_file1").get(0).files[0].size);
+	}
+	
+	function addFile() {
+		var count = $("input[name=file]").length;
+		var upload_form = $("#fileupload");
+		var label = $("<label></label>").attr("for", "id_file" + (count + 1));
+		label.text("파일업로드" + (count + 1));
+		upload_form.append(label);
+		var file = $("<input>").attr("type", "file");
+		file.attr("name", "file");
+		file.attr("id", "id_file" + (count + 1));
+		upload_form.append(file);
+		upload_form.append($("<br>"));
+	}
+</script>
 <body>
 	<c:url var="add" value="/board/add" />
-	<form action="${add }"	method="post">
+	<form action="${add }"	method="post" enctype="multipart/form-data">
 		<div>
 			<label for="id_title">제목</label>
 			<input id="id_title" type="text" name="title" required>
@@ -29,6 +50,22 @@
 		<div>
 			<label for="id_nodel">삭제금지</label>
 			<input id="id_nodel" type="checkbox" name="nodel">
+		</div>
+		<div id="fileupload">
+			<label for="id_file1">파일업로드1</label>
+			<input type="file" id="id_file1" name="file" multiple><br>
+			<!-- <label for="id_file2">파일업로드2</label>
+			<input type="file" id="id_file2" name="file"><br>
+			<label for="id_file3">파일업로드3</label>
+			<input type="file" id="id_file3" name="file"><br>
+			<label for="id_file4">파일업로드4</label>
+			<input type="file" id="id_file4" name="file"><br>
+			<label for="id_file5">파일업로드5</label>
+			<input type="file" id="id_file5" name="file"> -->
+		</div>
+		<div>
+			<button type="button" onclick="addFile();">업로드 추가</button>
+			<button type="button" onclick="fileCheck();">파일검사</button>
 		</div>
 		<div>
 			<button type="submit">저장</button>
